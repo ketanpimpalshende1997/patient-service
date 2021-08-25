3c645a331da876d03ba986514f73069353c1a1f1
Ketan pimpalshende <ketan.pimpalshende@citiustech.com>
2021-08-25 17:28:16 +0530

    patient service project added

---
+++ src/test/java/com/ct/patient/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ct.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
