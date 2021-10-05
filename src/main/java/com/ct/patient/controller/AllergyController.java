0a75926e506e3bc32d8c4041f736712bf4d63a29
Ketan pimpalshende <ketan.pimpalshende@citiustech.com>
2021-10-05 17:52:44 +0530

    cross origin added

--- src/main/java/com/ct/patient/controller/AllergyController.java
+++ src/main/java/com/ct/patient/controller/AllergyController.java
@@ -3,6 +3,7 @@ package com.ct.patient.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
+@CrossOrigin("*")
 public class AllergyController {
 
 	@Autowired
